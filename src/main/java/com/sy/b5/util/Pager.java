58d4de18363c90d10bc19faf9d43c9411260bf04
seongyeon-97 <creative9711@naver.com>
2021-11-03 17:39:26 +0900

    1

--- src/main/java/com/sy/b5/util/Pager.java
+++ src/main/java/com/sy/b5/util/Pager.java
@@ -13,9 +13,27 @@ public class Pager {
 	private Integer pn; //현재 페이지 번호
 	private Integer startRow; //시작 번호
 	
+	
 	public void makeRow() {
 		this.startRow = (this.getPn()-1)*this.getPerPage();
 	}
+	
+	public void makeNum(Long totalCount) { // db에서 받아온 totalCount를 매개변수로
+		// 2. 총 페이지 갯수 구하기
+		Long totalPage = totalCount/this.getPerPage();
+		if(totalCount/this.getPerPage() != 0) {
+			totalPage++;
+		}
+		
+		// 3. 총 블럭의 갯수를 구하기
+		Long perBlock = 5L; // 한 페이지에 5개씩 뿌려줄 예정
+		Long totalBlock = totalPage/perBlock;
+		if(totalPage%perBlock != 0) {
+			totalBlock++;
+		}
+	}
+	
+	// ---------------- setter, getter ------------------------
 	
 	// pn이 null이거나 1미만일 경우를 대비해 두 경우 모두 1로 return 받기
 	public Integer getPn() {
@@ -31,6 +49,8 @@ public class Pager {
 		}
 		return perPage;
 	}
+	
+	
 	
 	public String getSearch() {
 		if(this.search == null) {
